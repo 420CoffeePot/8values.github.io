dc3962e4d16248dd61d6e964c1468750160b5ee2
420CoffeePot <pot.coffee420@yandex.com>
2021-07-01 17:28:38 +0200

    changed variable names

--- ideologies.js
+++ ideologies.js
@@ -2,10 +2,10 @@ ideologies = [
     {
         "name": "Total Waifu Centrism",
         "stats": {
-            "econ": 50,
-            "dipl": 50,
-            "govt": 50,
-            "scty": 50
+            "appeal": 50,
+            "active": 50,
+            "human": 50,
+            "protect": 50
         }
     }
 ];
--- questions.js
+++ questions.js
@@ -2,10 +2,10 @@ questions = [
     {
         "question": "Overly shy waifus annoy me.",
         "effect": {
-            "econ": 11037,
-            "dipl": 0,
-            "govt": -69,
-            "scty": 0
+            "appeal":   0,
+            "active": -10,
+            "human":    0,
+            "protect": -5,
         }
     },
 
